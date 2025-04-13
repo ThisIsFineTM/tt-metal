564891f4c8a070f21d5bc2ae112c654131200e28
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-13 18:12:22 -0400

    nb move_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/data_movement/move/move_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/move/move_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::data_movement::detail {
 
-void py_bind_move(pybind11::module& m);
+namespace nb = nanobind;
+void bind_move(nb::module_& mod);
 
 }  // namespace ttnn::operations::data_movement::detail
