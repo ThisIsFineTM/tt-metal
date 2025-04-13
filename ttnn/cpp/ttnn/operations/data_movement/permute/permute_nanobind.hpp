6664cc1055d58d946c53a8fa3770e0e11711474a
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-13 18:33:27 -0400

    nb permute_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/data_movement/permute/permute_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/permute/permute_nanobind.hpp
@@ -4,16 +4,11 @@
 
 #pragma once
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-
-#include "cpp/pybind11/decorators.hpp"
-
-#include "permute.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::data_movement::detail {
-namespace py = pybind11;
 
-void bind_permute(py::module& module);
+namespace nb = nanobind;
+void bind_permute(nb::module_& mod);
 
 }  // namespace ttnn::operations::data_movement::detail
