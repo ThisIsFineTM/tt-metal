1fee2b3f9291f96ebb0cbacd5c7721ed736ca1b2
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-13 21:00:50 -0400

    nb interleaved_to_sharded_partial_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/data_movement/sharded_partial/interleaved_to_sharded_partial/interleaved_to_sharded_partial_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded_partial/interleaved_to_sharded_partial/interleaved_to_sharded_partial_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::data_movement {
 
-void py_bind_interleaved_to_sharded_partial(pybind11::module& module);
+namespace nb = nanobind;
+void bind_interleaved_to_sharded_partial(nb::module_& mod);
 
 }  // namespace ttnn::operations::data_movement
