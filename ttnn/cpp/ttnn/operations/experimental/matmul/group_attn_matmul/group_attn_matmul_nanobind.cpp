4a6c98d77f5b1d45fd25eba571a3596ae7d863dc
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-13 22:58:46 -0400

    nb group_attn_matmul_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/matmul/group_attn_matmul/group_attn_matmul_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/matmul/group_attn_matmul/group_attn_matmul_nanobind.cpp
@@ -2,22 +2,30 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "cpp/pybind11/decorators.hpp"
+#include "ttnn/operations/experimental/matmul/group_attn_matmul/group_attn_matmul_nanobind.hpp"
 
-#include "ttnn/operations/experimental/matmul/group_attn_matmul/group_attn_matmul_pybind.hpp"
+#include <optional>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "cpp/ttnn-nanobind/decorators.hpp"
+
 #include "ttnn/operations/experimental/matmul/group_attn_matmul/group_attn_matmul.hpp"
+
+
+namespace nb = nanobind;
 
 namespace ttnn::operations::experimental::matmul::detail {
 
-void bind_group_attn_matmul(pybind11::module& module) {
+void bind_group_attn_matmul(nb::module_& mod) {
     using OperationType = decltype(ttnn::experimental::group_attn_matmul);
     ttnn::bind_registered_operation(
-        module,
+        mod,
         ttnn::experimental::group_attn_matmul,
         R"doc(
             Performs a special pre-softmax matmul with [q_len, q_heads, batch, head_dim] and [batch, kv_heads, head_dim, kv_len]. q_len and kv_heads must be 1 and an intermediate value of [q_heads, batch, batch, kv_len] is produced (only on device cores). Batch dim from Z and Y is combined by taking the 1st, 2nd, ..., and 32nd row of Y from the batches in Z. Final output tensor is [1, q_heads, batch, kv_len]. In PyTorch, this is equivalent to: torch.matmul(A.transpose(0, 2), B).transpose(0, 2). Similar concept for post-softmax matmul.
         )doc",
-        ttnn::pybind_overload_t{
+        ttnn::nanobind_overload_t{
             [](const OperationType& self,
                const Tensor& input_tensor_a,
                const Tensor& input_tensor_b,
@@ -37,15 +45,15 @@ void bind_group_attn_matmul(pybind11::module& module) {
                     compute_kernel_config,
                     optional_output_tensor);
             },
-            pybind11::arg().noconvert(),
-            pybind11::arg().noconvert(),
-            pybind11::kw_only(),
-            pybind11::arg("compute_with_storage_grid_size").noconvert(),
-            pybind11::arg("memory_config").noconvert() = tt::tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG,
-            pybind11::arg("dtype").noconvert() = std::nullopt,
-            pybind11::arg("compute_kernel_config").noconvert() = std::nullopt,
-            pybind11::arg("optional_output_tensor").noconvert() = std::nullopt,
-            pybind11::arg("queue_id") = DefaultQueueId});
+            nb::arg().noconvert(),
+            nb::arg().noconvert(),
+            nb::kw_only(),
+            nb::arg("compute_with_storage_grid_size").noconvert(),
+            nb::arg("memory_config").noconvert() = tt::tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG,
+            nb::arg("dtype").noconvert() = std::nullopt,
+            nb::arg("compute_kernel_config").noconvert() = std::nullopt,
+            nb::arg("optional_output_tensor").noconvert() = std::nullopt,
+            nb::arg("queue_id") = DefaultQueueId});
 }
 
 }  // namespace ttnn::operations::experimental::matmul::detail
