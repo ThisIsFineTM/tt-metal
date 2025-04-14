69374a413db3b15e8e93ce95529e27ea8e062666
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-13 21:00:21 -0400

    nb sharded_to_interleaved_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/data_movement/sharded/sharded_to_interleaved/sharded_to_interleaved_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/sharded_to_interleaved/sharded_to_interleaved_nanobind.cpp
@@ -2,10 +2,13 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include "sharded_to_interleaved_nanobind.hpp"
 
-#include "cpp/pybind11/decorators.hpp"
+#include <optional>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "cpp/ttnn-nanobind/decorators.hpp"
 #include "sharded_to_interleaved.hpp"
 #include "ttnn/types.hpp"
 
@@ -17,12 +20,12 @@ namespace detail {
 
 template <typename data_movement_sharded_operation_t>
 void bind_sharded_to_interleaved(
-    pybind11::module& module, const data_movement_sharded_operation_t& operation, const char* doc) {
+    nb::module_& mod, const data_movement_sharded_operation_t& operation, const char* doc) {
     bind_registered_operation(
-        module,
+        mod,
         operation,
         doc,
-        ttnn::pybind_overload_t{
+        ttnn::nanobind_overload_t{
             [](const data_movement_sharded_operation_t& self,
                const ttnn::Tensor& input_tensor,
                const std::optional<MemoryConfig>& memory_config,
@@ -36,21 +39,21 @@ void bind_sharded_to_interleaved(
                     output_dtype,
                     is_l1_aligned);
             },
-            py::arg("input_tensor").noconvert(),
-            py::arg("memory_config") = std::nullopt,
-            py::arg("output_dtype") = std::nullopt,
-            py::kw_only(),
-            py::arg("queue_id") = DefaultQueueId,
-            py::arg("is_l1_aligned") = false,
+            nb::arg("input_tensor").noconvert(),
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("output_dtype") = std::nullopt,
+            nb::kw_only(),
+            nb::arg("queue_id") = DefaultQueueId,
+            nb::arg("is_l1_aligned") = false,
         });
 }
 
 }  // namespace detail
 
 // TODO: Add more descriptions to the arguments
-void py_bind_sharded_to_interleaved(pybind11::module& module) {
+void bind_sharded_to_interleaved(nb::module_& mod) {
     detail::bind_sharded_to_interleaved(
-        module,
+        mod,
         ttnn::sharded_to_interleaved,
         R"doc(sharded_to_interleaved(input_tensor: ttnn.Tensor,  memory_config: ttnn.MemoryConfig, *,  queue_id: int) -> ttnn.Tensor
 
