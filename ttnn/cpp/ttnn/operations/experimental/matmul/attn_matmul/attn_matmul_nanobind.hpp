a17142c744f9de3ecdaddf5f815d5648e3a3cb9c
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-17 00:21:44 -0400

    nb attn_matmul_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/matmul/attn_matmul/attn_matmul_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/matmul/attn_matmul/attn_matmul_nanobind.hpp
@@ -4,11 +4,13 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::matmul::detail {
 
-void bind_attn_matmul(pybind11::module& module);
-void bind_attn_matmul_from_cache(pybind11::module& module);
+namespace nb = nanobind;
+
+void bind_attn_matmul(nb::module_& mod);
+void bind_attn_matmul_from_cache(nb::module_& mod);
 
 }  // namespace ttnn::operations::experimental::matmul::detail
