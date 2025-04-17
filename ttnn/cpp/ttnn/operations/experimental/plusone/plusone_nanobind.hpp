cd2161b349ad0907a1725b3c0be712653f973955
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-17 00:30:12 -0400

    nb plusone_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/plusone/plusone_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/plusone/plusone_nanobind.hpp
@@ -3,10 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
-#include "pybind11/pybind_fwd.hpp"
+
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::plusone::detail {
-namespace py = pybind11;
-void bind_experimental_plusone_operation(py::module& module);
+
+namespace nb = nanobind;
+
+void bind_experimental_plusone_operation(nb::module_& mod);
 
 }  // namespace ttnn::operations::experimental::plusone::detail
