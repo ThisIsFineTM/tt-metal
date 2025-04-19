f95b61ac7a3e569c3c407c374808edb72343f5ef
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-18 21:31:48 -0400

    nb experimental hc_sum_reduce_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/ssm/hc_sum_reduce/hc_sum_reduce_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ssm/hc_sum_reduce/hc_sum_reduce_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::ssm::detail {
 
-void bind_hc_sum_reduce(pybind11::module& module);
+namespace nb = nanobind;
+void bind_hc_sum_reduce(nb::module_& mod);
 
 }  // namespace ttnn::operations::experimental::ssm::detail
