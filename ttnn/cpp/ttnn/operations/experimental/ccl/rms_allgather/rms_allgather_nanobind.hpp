71ac933ae0a77f7ae4e04500026c6e5bf1d288f6
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-18 23:25:21 -0400

    nb experimental rms_allgather_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/ccl/rms_allgather/rms_allgather_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/rms_allgather/rms_allgather_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::ccl {
 
-void bind_fused_rms_1_1_32_8192(pybind11::module& module);
+namespace nb = nanobind;
+void bind_fused_rms_1_1_32_8192(nb::module_& mod);
 
 }  // namespace ttnn::operations::experimental::ccl
