d24c48ef50b2ce7fec515af21ae0f41e4347244f
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-18 22:23:55 -0400

    nb experimental nlp_concat_heads_decode_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/transformer/nlp_concat_heads_decode/nlp_concat_heads_decode_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/nlp_concat_heads_decode/nlp_concat_heads_decode_nanobind.cpp
@@ -2,23 +2,30 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "cpp/pybind11/decorators.hpp"
-
 #include "ttnn/operations/experimental/transformer/nlp_concat_heads_decode/nlp_concat_heads_decode.hpp"
+
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "cpp/ttnn-nanobind/decorators.hpp"
+
+namespace nb = nanobind;
 
 namespace ttnn::operations::experimental::transformer::detail {
 
-namespace py = pybind11;
+void bind_nlp_concat_heads_decode(nb::module_& mod) {
 
-void bind_nlp_concat_heads_decode(py::module& module) {
     using OperationType = decltype(ttnn::experimental::nlp_concat_heads_decode);
+
     ttnn::bind_registered_operation(
-        module,
+        mod,
         ttnn::experimental::nlp_concat_heads_decode,
         R"doc(
             Shuffles [S=1, B=32, 32(num_heads), head_dim] tensor into tensor with shape [S=1, 1, B=32, num_heads * head_dim]. num_heads should be specified and be less than 32; the op will assume the input padded num_heads to 32 and will unpad it. The output is default width sharded by num heads.
         )doc",
-        ttnn::pybind_overload_t{
+        ttnn::nanobind_overload_t{
             [](const OperationType& self,
                const ttnn::Tensor& input_tensor,
                const uint32_t num_heads,
@@ -27,12 +34,12 @@ void bind_nlp_concat_heads_decode(py::module& module) {
                QueueId queue_id) {
                 return self(queue_id, input_tensor, num_heads, memory_config, optional_output_tensor);
             },
-            py::arg("input_tensor").noconvert(),
-            py::kw_only(),
-            py::arg("num_heads").noconvert(),
-            py::arg("memory_config") = std::nullopt,
-            py::arg("output_tensor") = std::nullopt,
-            py::arg("queue_id") = DefaultQueueId});
+            nb::arg("input_tensor").noconvert(),
+            nb::kw_only(),
+            nb::arg("num_heads").noconvert(),
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("output_tensor") = std::nullopt,
+            nb::arg("queue_id") = DefaultQueueId});
 }
 
 }  // namespace ttnn::operations::experimental::transformer::detail
