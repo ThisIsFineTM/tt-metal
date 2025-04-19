a160023eb87d7dfcee9d51484da7eeb2f96ec36e
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-18 22:00:53 -0400

    nb experimental nlp_create_qkv_heads_falcon7b_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/transformer/nlp_create_qkv_heads_falcon7b/nlp_create_qkv_heads_falcon7b_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/nlp_create_qkv_heads_falcon7b/nlp_create_qkv_heads_falcon7b_nanobind.hpp
@@ -4,9 +4,10 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::transformer::detail {
 
-void bind_nlp_create_qkv_heads_falcon7b(pybind11::module& module);
+namespace nb = nanobind;
+void bind_nlp_create_qkv_heads_falcon7b(nb::module_& mod);
 }  // namespace ttnn::operations::experimental::transformer::detail
