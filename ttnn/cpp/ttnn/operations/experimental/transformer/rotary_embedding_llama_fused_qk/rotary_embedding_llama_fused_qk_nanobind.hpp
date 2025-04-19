8ee2b00a5d4885517b7002b55b28cf727b891b41
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-18 22:45:01 -0400

    nb experimental rotary_embedding_llama_fused_qk_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama_fused_qk/rotary_embedding_llama_fused_qk_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama_fused_qk/rotary_embedding_llama_fused_qk_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::transformer {
 
-void py_bind_rotary_embedding_llama_fused_qk(pybind11::module& module);
+namespace nb = nanobind;
+void bind_rotary_embedding_llama_fused_qk(nb::module_& mod);
 
 }  // namespace ttnn::operations::experimental::transformer
