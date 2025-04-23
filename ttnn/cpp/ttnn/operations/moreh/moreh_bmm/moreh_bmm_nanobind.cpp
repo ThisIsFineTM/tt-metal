0aaef3a770e3585fa9cc44f49461aa5ebde7df68
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-22 20:38:47 -0400

    nb moreh_bmm_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_bmm/moreh_bmm_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_bmm/moreh_bmm_nanobind.cpp
@@ -2,23 +2,29 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_bmm_pybind.hpp"
+#include "moreh_bmm_nanobind.hpp"
+
+#include <optional>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
 
 #include "moreh_bmm.hpp"
-#include "pybind11/decorators.hpp"
+#include "ttnn-nanobind/decorators.hpp"
 
 namespace ttnn::operations::moreh::moreh_bmm {
-void bind_moreh_bmm_operation(py::module& module) {
+
+void bind_moreh_bmm_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_bmm,
         "Moreh BMM Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("input"),
-            py::arg("mat2"),
-            py::kw_only(),
-            py::arg("output") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("input"),
+            nb::arg("mat2"),
+            nb::kw_only(),
+            nb::arg("output") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
+
 }  // namespace ttnn::operations::moreh::moreh_bmm
--- ttnn/cpp/ttnn/operations/moreh/moreh_bmm/moreh_bmm_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_bmm/moreh_bmm_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
-
-namespace py = pybind11;
+#include "nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::moreh::moreh_bmm {
-void bind_moreh_bmm_operation(py::module& module);
+
+namespace nb = nanobind;
+void bind_moreh_bmm_operation(nb::module_& mod);
+
 }  // namespace ttnn::operations::moreh::moreh_bmm
