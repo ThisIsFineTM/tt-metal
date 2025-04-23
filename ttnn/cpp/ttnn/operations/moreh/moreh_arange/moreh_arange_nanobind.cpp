20295f4f53c3288be79303b6b21c7880ac2513c1
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-22 20:40:53 -0400

    nb moreh_arange_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_arange/moreh_arange_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_arange/moreh_arange_nanobind.cpp
@@ -2,27 +2,32 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_arange_pybind.hpp"
+#include "moreh_arange_nanobind.hpp"
+
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
 
 #include "moreh_arange.hpp"
-#include "pybind11/decorators.hpp"
+#include "ttnn-nanobind/decorators.hpp"
 
 namespace ttnn::operations::moreh::moreh_arange {
-void bind_moreh_arange_operation(py::module& module) {
+void bind_moreh_arange_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_arange,
         "Moreh Arange Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("start") = 0,
-            py::arg("end"),
-            py::arg("step") = 1,
-            py::arg("any"),
-            py::kw_only(),
-            py::arg("output") = std::nullopt,
-            py::arg("untilize_out") = false,
-            py::arg("dtype") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
+        ttnn::nanobind_arguments_t{
+            nb::arg("start") = 0,
+            nb::arg("end"),
+            nb::arg("step") = 1,
+            nb::arg("any"),
+            nb::kw_only(),
+            nb::arg("output") = std::nullopt,
+            nb::arg("untilize_out") = false,
+            nb::arg("dtype") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
         });
 }
 }  // namespace ttnn::operations::moreh::moreh_arange
