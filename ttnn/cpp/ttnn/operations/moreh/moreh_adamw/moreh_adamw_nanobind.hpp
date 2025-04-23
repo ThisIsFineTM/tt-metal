4d66840efdbe6fe39fa93e8f582b176063757c65
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-22 22:06:15 -0400

    nb moreh_adamw_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_adamw/moreh_adamw_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_adamw/moreh_adamw_nanobind.hpp
@@ -4,15 +4,12 @@
 
 #pragma once
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-
-namespace py = pybind11;
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::moreh::moreh_adamw {
 
-void bind_moreh_adamw_operation(py::module& module);
+void bind_moreh_adamw_operation(nb::module_& mod);
 
-void py_module(py::module& module);
+void py_module(nb::module_& mod);
 
 }  // namespace ttnn::operations::moreh::moreh_adamw
