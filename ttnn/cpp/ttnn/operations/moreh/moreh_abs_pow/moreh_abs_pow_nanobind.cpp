8ea8d8733cb39656533807eb0ec8d550165f9d40
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-22 20:31:05 -0400

    nb moreh_abs_pow_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_abs_pow/moreh_abs_pow_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_abs_pow/moreh_abs_pow_nanobind.cpp
@@ -2,24 +2,30 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_abs_pow_pybind.hpp"
+#include "moreh_abs_pow_nanobind.hpp"
+
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
 
 #include "moreh_abs_pow.hpp"
-#include "pybind11/decorators.hpp"
+#include "ttnn-nanobind/decorators.hpp"
 
 namespace ttnn::operations::moreh::moreh_abs_pow {
-void bind_moreh_abs_pow_operation(py::module& module) {
+
+void bind_moreh_abs_pow_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_abs_pow,
         "Moreh Pow Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("input"),
-            py::arg("p"),
-            py::kw_only(),
-            py::arg("output") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt,
+        ttnn::nanobind_arguments_t{
+            nb::arg("input"),
+            nb::arg("p"),
+            nb::kw_only(),
+            nb::arg("output") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt,
         });
 }
 }  // namespace ttnn::operations::moreh::moreh_abs_pow
