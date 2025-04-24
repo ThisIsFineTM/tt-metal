d97ca7ca7ba36b1a5cd7fd85eaf97450d681d4b6
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 22:20:47 -0400

    nb moreh_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_nanobind.hpp
@@ -3,10 +3,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
-#include "pybind11/pybind_fwd.hpp"
 
-namespace py = pybind11;
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::moreh {
-void bind_moreh_operations(py::module& module);
+
+namespace nb = nanobind;
+void bind_moreh_operations(nb::module_& mod);
 }  // namespace ttnn::operations::moreh
