5398382cea64323b015755873a4f6972ed7c0436
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 21:32:54 -0400

    nb moreh_linear_backward_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_linear_backward/moreh_linear_backward_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_linear_backward/moreh_linear_backward_nanobind.cpp
@@ -2,31 +2,40 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_linear_backward_pybind.hpp"
+#include "moreh_linear_backward_nanobind.hpp"
 
-#include "pybind11/decorators.hpp"
+#include <optional>
+#include <vector>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+#include <nanobind/stl/vector.h>
+
+#include "ttnn-nanobind/decorators.hpp"
 #include "ttnn/operations/moreh/moreh_linear_backward/moreh_linear_backward.hpp"
 
+namespace nb = nanobind;
+
 namespace ttnn::operations::moreh::moreh_linear_backward {
-void bind_moreh_linear_backward_operation(py::module& module) {
+void bind_moreh_linear_backward_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_linear_backward,
         "Moreh Linear Backward Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("output_grad"),
-            py::arg("input"),
-            py::arg("weight"),
-            py::arg("are_required_outputs") = std::vector<bool>{true, true, true},
+        ttnn::nanobind_arguments_t{
+            nb::arg("output_grad"),
+            nb::arg("input"),
+            nb::arg("weight"),
+            nb::arg("are_required_outputs") = std::vector<bool>{true, true, true},
 
-            py::arg("bias") = std::nullopt,
-            py::arg("input_grad") = std::nullopt,
-            py::arg("weight_grad") = std::nullopt,
-            py::arg("bias_grad") = std::nullopt,
+            nb::arg("bias") = std::nullopt,
+            nb::arg("input_grad") = std::nullopt,
+            nb::arg("weight_grad") = std::nullopt,
+            nb::arg("bias_grad") = std::nullopt,
 
-            py::arg("input_grad_memory_config") = std::nullopt,
-            py::arg("weight_grad_memory_config") = std::nullopt,
-            py::arg("bias_grad_memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+            nb::arg("input_grad_memory_config") = std::nullopt,
+            nb::arg("weight_grad_memory_config") = std::nullopt,
+            nb::arg("bias_grad_memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 }  // namespace ttnn::operations::moreh::moreh_linear_backward
