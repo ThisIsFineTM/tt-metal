b004570a94dc86d96641724f2d715c4fae8cf776
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 21:44:34 -0400

    nb moreh_mean_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_mean/moreh_mean_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_mean/moreh_mean_nanobind.cpp
@@ -2,25 +2,32 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_mean_pybind.hpp"
+#include "moreh_mean_nanobind.hpp"
 
-#include "pybind11/decorators.hpp"
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "ttnn-nanobind/decorators.hpp"
 #include "ttnn/operations/moreh/moreh_mean/moreh_mean.hpp"
 
+namespace nb = nanobind;
+
 namespace ttnn::operations::moreh::moreh_mean {
-void bind_moreh_mean_operation(py::module& module) {
+void bind_moreh_mean_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_mean,
         "Moreh Mean Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("input"),
-            py::kw_only(),
-            py::arg("dim"),
-            py::arg("keepdim") = false,
-            py::arg("divisor") = std::nullopt,
-            py::arg("output") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("input"),
+            nb::kw_only(),
+            nb::arg("dim"),
+            nb::arg("keepdim") = false,
+            nb::arg("divisor") = std::nullopt,
+            nb::arg("output") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 }  // namespace ttnn::operations::moreh::moreh_mean
