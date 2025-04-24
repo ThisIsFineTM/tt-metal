1ada07bebc243498fb7fbc718cdc3946193b554c
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 22:05:32 -0400

    nb moreh_sgd_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_sgd/moreh_sgd_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_sgd/moreh_sgd_nanobind.cpp
@@ -2,32 +2,39 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_sgd_pybind.hpp"
+#include "moreh_sgd_nanobind.hpp"
 
-#include "pybind11/decorators.hpp"
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "ttnn-nanobind/decorators.hpp"
 #include "ttnn/operations/moreh/moreh_sgd/moreh_sgd.hpp"
 
+namespace nb = nanobind;
+
 namespace ttnn::operations::moreh::moreh_sgd {
-void bind_moreh_sgd_operation(py::module& module) {
+void bind_moreh_sgd_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_sgd,
         "Moreh SGD Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("param_in"),
-            py::arg("grad"),
-            py::arg("momentum_buffer_in") = std::nullopt,
-            py::arg("param_out") = std::nullopt,
-            py::arg("momentum_buffer_out") = std::nullopt,
-            py::arg("lr") = 1e-3,
-            py::arg("momentum") = 0,
-            py::arg("dampening") = 0,
-            py::arg("weight_decay") = 0,
-            py::arg("nesterov") = false,
-            py::kw_only(),
-            py::arg("momentum_initialized"),
-            py::arg("param_out_memory_config") = std::nullopt,
-            py::arg("momentum_buffer_out_memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("param_in"),
+            nb::arg("grad"),
+            nb::arg("momentum_buffer_in") = std::nullopt,
+            nb::arg("param_out") = std::nullopt,
+            nb::arg("momentum_buffer_out") = std::nullopt,
+            nb::arg("lr") = 1e-3,
+            nb::arg("momentum") = 0,
+            nb::arg("dampening") = 0,
+            nb::arg("weight_decay") = 0,
+            nb::arg("nesterov") = false,
+            nb::kw_only(),
+            nb::arg("momentum_initialized"),
+            nb::arg("param_out_memory_config") = std::nullopt,
+            nb::arg("momentum_buffer_out_memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 }  // namespace ttnn::operations::moreh::moreh_sgd
