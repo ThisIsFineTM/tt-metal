0fa1e463a6d026cd7b345934740ef1c15c8780c5
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 22:30:57 -0400

    nb layernorm_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/normalization/layernorm/layernorm_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/layernorm_nanobind.cpp
@@ -2,35 +2,44 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "layernorm_pybind.hpp"
-#include "cpp/pybind11/decorators.hpp"
+#include "layernorm_nanobind.hpp"
+
+#include <optional>
+
+#include <fmt/format.h>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "cpp/ttnn-nanobind/decorators.hpp"
 #include "layernorm.hpp"
+
+namespace nb = nanobind;
 
 namespace ttnn::operations::normalization::detail {
 
-namespace py = pybind11;
+void bind_normalization_layernorm_program_config(nb::module_& mod) {
+    nb::class_<LayerNormProgramConfig>(mod, "LayerNormProgramConfig")
+        .def(nb::init<>());
 
-void bind_normalization_layernorm_program_config(py::module& module) {
-    py::class_<LayerNormProgramConfig>(module, "LayerNormProgramConfig").def(py::init<>());
+    nb::class_<LayerNormDefaultProgramConfig>(mod, "LayerNormDefaultProgramConfig")
+        .def(nb::init<>());
 
-    py::class_<LayerNormDefaultProgramConfig>(module, "LayerNormDefaultProgramConfig").def(py::init<>());
-
-    py::class_<LayerNormShardedMultiCoreProgramConfig>(module, "LayerNormShardedMultiCoreProgramConfig")
+    nb::class_<LayerNormShardedMultiCoreProgramConfig>(mod, "LayerNormShardedMultiCoreProgramConfig")
         .def(
-            py::init<CoreCoord, std::size_t, std::size_t, std::size_t, bool>(),
-            py::kw_only(),
-            py::arg("compute_with_storage_grid_size"),
-            py::arg("subblock_w").noconvert(),
-            py::arg("block_h").noconvert(),
-            py::arg("block_w").noconvert(),
-            py::arg("inplace").noconvert())
+            nb::init<CoreCoord, std::size_t, std::size_t, std::size_t, bool>(),
+            nb::kw_only(),
+            nb::arg("compute_with_storage_grid_size"),
+            nb::arg("subblock_w").noconvert(),
+            nb::arg("block_h").noconvert(),
+            nb::arg("block_w").noconvert(),
+            nb::arg("inplace").noconvert())
         .def(
             "__repr__", [](const LayerNormShardedMultiCoreProgramConfig& config) { return fmt::format("{}", config); });
 }
 
-void bind_normalization_layernorm_operation(py::module& module) {
+void bind_normalization_layernorm_operation(nb::module_& mod) {
     ttnn::bind_registered_operation(
-        module,
+        mod,
         ttnn::layer_norm,
         R"doc(
             Compute layer_norm over :attr:`input_tensor`.
@@ -56,21 +65,21 @@ void bind_normalization_layernorm_operation(py::module& module) {
 
         )doc",
 
-        ttnn::pybind_arguments_t{
-            py::arg("input_tensor"),
-            py::kw_only(),
-            py::arg("epsilon") = 1e-12,
-            py::arg("weight") = std::nullopt,
-            py::arg("bias") = std::nullopt,
-            py::arg("residual_input_tensor") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("program_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("input_tensor"),
+            nb::kw_only(),
+            nb::arg("epsilon") = 1e-12,
+            nb::arg("weight") = std::nullopt,
+            nb::arg("bias") = std::nullopt,
+            nb::arg("residual_input_tensor") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("program_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 
-void bind_normalization_layernorm(py::module& module) {
-    bind_normalization_layernorm_program_config(module);
-    bind_normalization_layernorm_operation(module);
+void bind_normalization_layernorm(nb::module_& mod) {
+    bind_normalization_layernorm_program_config(mod);
+    bind_normalization_layernorm_operation(mod);
 }
 
 }  // namespace ttnn::operations::normalization::detail
--- ttnn/cpp/ttnn/operations/normalization/layernorm/layernorm_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/layernorm_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::normalization::detail {
 
-void bind_normalization_layernorm(pybind11::module& module);
+namespace nb = nanobind;
+void bind_normalization_layernorm(nb::module_& mod);
 
 }  // namespace ttnn::operations::normalization::detail
