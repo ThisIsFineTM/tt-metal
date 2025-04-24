50597eb913b1298d5b8c919de96c5a8d1806f161
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 21:26:09 -0400

    nb moreh_group_norm_backward_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_group_norm_backward/moreh_group_norm_backward_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_group_norm_backward/moreh_group_norm_backward_nanobind.cpp
@@ -2,33 +2,43 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_group_norm_backward_pybind.hpp"
+#include "moreh_group_norm_backward_nanobind.hpp"
+
+#include <optional>
+#include <vector>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+#include <nanobind/stl/vector.h>
 
 #include "moreh_group_norm_backward.hpp"
-#include "pybind11/decorators.hpp"
+#include "ttnn-nanobind/decorators.hpp"
+
+namespace nb = nanobind;
 
 namespace ttnn::operations::moreh::moreh_group_norm_backward {
-void bind_moreh_group_norm_backward_operation(py::module& module) {
+
+void bind_moreh_group_norm_backward_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_group_norm_backward,
         "Moreh Group Norm Backward Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("output_grad"),
-            py::arg("input"),
-            py::arg("mean"),
-            py::arg("rstd"),
-            py::arg("num_groups"),
-            py::kw_only(),
-            py::arg("are_required_outputs") = std::vector<bool>{true, false, false},
-            py::arg("gamma") = std::nullopt,
-            py::arg("input_grad") = std::nullopt,
-            py::arg("gamma_grad") = std::nullopt,
-            py::arg("beta_grad") = std::nullopt,
-            py::arg("input_grad_memory_config") = std::nullopt,
-            py::arg("gamma_grad_memory_config") = std::nullopt,
-            py::arg("beta_grad_memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt
+        ttnn::nanobind_arguments_t{
+            nb::arg("output_grad"),
+            nb::arg("input"),
+            nb::arg("mean"),
+            nb::arg("rstd"),
+            nb::arg("num_groups"),
+            nb::kw_only(),
+            nb::arg("are_required_outputs") = std::vector<bool>{true, false, false},
+            nb::arg("gamma") = std::nullopt,
+            nb::arg("input_grad") = std::nullopt,
+            nb::arg("gamma_grad") = std::nullopt,
+            nb::arg("beta_grad") = std::nullopt,
+            nb::arg("input_grad_memory_config") = std::nullopt,
+            nb::arg("gamma_grad_memory_config") = std::nullopt,
+            nb::arg("beta_grad_memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt
 
         });
 }
