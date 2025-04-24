9235c95dd67fea49f707d151b778acc1547c803e
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 21:50:35 -0400

    nb moreh_nll_loss_backward_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_nll_loss_backward/moreh_nll_loss_backward_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_nll_loss_backward/moreh_nll_loss_backward_nanobind.cpp
@@ -2,37 +2,38 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_nll_loss_backward_pybind.hpp"
+#include "moreh_nll_loss_backward_nanobind.hpp"
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
 
 #include "moreh_nll_loss_backward.hpp"
-#include "pybind11/cast.h"
-#include "cpp/pybind11/decorators.hpp"
+#include "cpp/ttnn-nanobind/decorators.hpp"
 
-namespace py = pybind11;
+namespace nb = nanobind;
 
 namespace ttnn::operations::moreh::moreh_nll_loss_backward {
 
-void bind_moreh_nll_loss_backward_operation(py::module& module) {
+void bind_moreh_nll_loss_backward_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_nll_loss_backward,
         R"doc(moreh_nll_loss_backward(target_tensor: ttnn.Tensor, weight_tensor: Optional[ttnn.Tensor], output_grad_tensor: ttnn.Tensor, input_grad_tensor: Optional[ttnn.Tensor], ignore_index: int, memory_config: Optional[ttnn.MemoryConfig] = None, compute_kernel_config: Optional[DeviceComputeKernelConfig]) -> ttnn.Tensor
             Compute backward for nll_loss operation with reduction set to None
         )doc",
-        ttnn::pybind_arguments_t{
-            py::arg("target_tensor"),
-            py::arg("output_grad_tensor"),
-            py::arg("reduction_mean"),
-            py::kw_only(),
-            py::arg("weight_tensor") = std::nullopt,
-            py::arg("input_grad_tensor") = std::nullopt,
-            py::arg("divisor_tensor") = std::nullopt,
-            py::arg("ignore_index") = -100,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("target_tensor"),
+            nb::arg("output_grad_tensor"),
+            nb::arg("reduction_mean"),
+            nb::kw_only(),
+            nb::arg("weight_tensor") = std::nullopt,
+            nb::arg("input_grad_tensor") = std::nullopt,
+            nb::arg("divisor_tensor") = std::nullopt,
+            nb::arg("ignore_index") = -100,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 
 }  // namespace ttnn::operations::moreh::moreh_nll_loss_backward
