aafce23c344f273e8b99f48f6efe0f8f0239a729
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 21:47:15 -0400

    nb moreh_matmul_backward_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_matmul_backward/moreh_matmul_backward_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_matmul_backward/moreh_matmul_backward_nanobind.cpp
@@ -2,27 +2,35 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_matmul_backward_pybind.hpp"
+#include "moreh_matmul_backward_nanobind.hpp"
 
-#include "pybind11/cast.h"
-#include "pybind11/decorators.hpp"
+#include <optional>
+#include <vector>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+#include <nanobind/stl/vector.h>
+
+#include "ttnn-nanobind/decorators.hpp"
 #include "ttnn/operations/moreh/moreh_matmul_backward/moreh_matmul_backward.hpp"
 
+namespace nb = nanobind;
+
 namespace ttnn::operations::moreh::moreh_matmul_backward {
-void bind_moreh_matmul_backward_operation(py::module& module) {
+void bind_moreh_matmul_backward_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_matmul_backward,
         "Moreh Matmul Backward Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("output_grad"),
-            py::arg("input_a"),
-            py::arg("input_b"),
-            py::kw_only(),
-            py::arg("are_required_outputs") = std::vector<bool>{true, true},
-            py::arg("input_a_grad") = std::nullopt,
-            py::arg("input_b_grad") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("output_grad"),
+            nb::arg("input_a"),
+            nb::arg("input_b"),
+            nb::kw_only(),
+            nb::arg("are_required_outputs") = std::vector<bool>{true, true},
+            nb::arg("input_a_grad") = std::nullopt,
+            nb::arg("input_b_grad") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 }  // namespace ttnn::operations::moreh::moreh_matmul_backward
