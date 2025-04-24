02c6025acf9d1c260d7c2e4309da2cda6407ec6c
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 22:23:46 -0400

    nb moreh_sum_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_sum/moreh_sum_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_sum/moreh_sum_nanobind.cpp
@@ -2,24 +2,29 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_sum_pybind.hpp"
+#include "moreh_sum_nanobind.hpp"
 
-#include "pybind11/decorators.hpp"
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "ttnn-nanobind/decorators.hpp"
 #include "ttnn/operations/moreh/moreh_sum/moreh_sum.hpp"
 
 namespace ttnn::operations::moreh::moreh_sum {
-void bind_moreh_sum_operation(py::module& module) {
+void bind_moreh_sum_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_sum,
         "Moreh Sum Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("input"),
-            py::arg("dim"),
-            py::kw_only(),
-            py::arg("keepdim") = false,
-            py::arg("output") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("input"),
+            nb::arg("dim"),
+            nb::kw_only(),
+            nb::arg("keepdim") = false,
+            nb::arg("output") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 }  // namespace ttnn::operations::moreh::moreh_sum
