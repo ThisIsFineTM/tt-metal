7236434e78125a88ef9926f6c755462aba1dd3d8
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-24 19:10:19 -0400

    nb transformer_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/transformer/transformer_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/transformer/transformer_nanobind.cpp
@@ -2,44 +2,47 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "transformer_pybind.hpp"
+#include "transformer_nanobind.hpp"
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <cstddef>
+#include <optional>
 
-#include "attention_softmax/attention_softmax_pybind.hpp"
-#include "concatenate_heads/concatenate_heads_pybind.hpp"
-#include "sdpa/sdpa_pybind.hpp"
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "attention_softmax/attention_softmax_nanobind.hpp"
+#include "concatenate_heads/concatenate_heads_nanobind.hpp"
+#include "sdpa/sdpa_nanobind.hpp"
 #include "sdpa_config.hpp"
-#include "sdpa_decode/sdpa_decode_pybind.hpp"
-#include "split_query_key_value_and_split_heads/split_query_key_value_and_split_heads_pybind.hpp"
+#include "sdpa_decode/sdpa_decode_nanobind.hpp"
+#include "split_query_key_value_and_split_heads/split_query_key_value_and_split_heads_nanobind.hpp"
+
+namespace nb = nanobind;
 
 namespace ttnn::operations::transformer {
 
-namespace py = pybind11;
-
-void py_module(py::module& module) {
-    py::class_<SDPAProgramConfig>(module, "SDPAProgramConfig")
+void py_module(nb::module_& mod) {
+    nb::class_<SDPAProgramConfig>(mod, "SDPAProgramConfig")
         .def(
-            py::init<CoreCoord, std::optional<CoreRangeSet>, std::size_t, std::size_t, std::optional<bool>>(),
-            py::kw_only(),
-            py::arg("compute_with_storage_grid_size"),
-            py::arg("sub_core_grids") = std::nullopt,
-            py::arg("q_chunk_size").noconvert(),
-            py::arg("k_chunk_size").noconvert(),
-            py::arg("exp_approx_mode") = std::nullopt)
-        .def_readwrite("compute_with_storage_grid_size", &SDPAProgramConfig::compute_with_storage_grid_size)
-        .def_readwrite("sub_core_grids", &SDPAProgramConfig::sub_core_grids)
-        .def_readwrite("q_chunk_size", &SDPAProgramConfig::q_chunk_size)
-        .def_readwrite("k_chunk_size", &SDPAProgramConfig::k_chunk_size)
-        .def_readwrite("exp_approx_mode", &SDPAProgramConfig::exp_approx_mode);
+            nb::init<CoreCoord, std::optional<CoreRangeSet>, std::size_t, std::size_t, std::optional<bool>>(),
+            nb::kw_only(),
+            nb::arg("compute_with_storage_grid_size"),
+            nb::arg("sub_core_grids") = std::nullopt,
+            nb::arg("q_chunk_size").noconvert(),
+            nb::arg("k_chunk_size").noconvert(),
+            nb::arg("exp_approx_mode") = std::nullopt)
+        .def_rw("compute_with_storage_grid_size", &SDPAProgramConfig::compute_with_storage_grid_size)
+        .def_rw("sub_core_grids", &SDPAProgramConfig::sub_core_grids)
+        .def_rw("q_chunk_size", &SDPAProgramConfig::q_chunk_size)
+        .def_rw("k_chunk_size", &SDPAProgramConfig::k_chunk_size)
+        .def_rw("exp_approx_mode", &SDPAProgramConfig::exp_approx_mode);
 
-    py_bind_attention_softmax(module);
-    py_bind_concatenate_heads(module);
-    py_bind_split_query_key_value_and_split_heads(module);
+    bind_attention_softmax(mod);
+    bind_concatenate_heads(mod);
+    bind_split_query_key_value_and_split_heads(mod);
 
-    py_bind_sdpa(module);
-    py_bind_sdpa_decode(module);
+    bind_sdpa(mod);
+    bind_sdpa_decode(mod);
 }
 
 }  // namespace ttnn::operations::transformer
