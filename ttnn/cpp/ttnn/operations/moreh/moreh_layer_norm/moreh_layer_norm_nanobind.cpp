7b64eadc0dc02b5f183939e39be853c030e0b985
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 20:01:41 -0400

    nb moreh_layer_norm_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_layer_norm/moreh_layer_norm_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_layer_norm/moreh_layer_norm_nanobind.cpp
@@ -2,29 +2,36 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_layer_norm_pybind.hpp"
+#include "moreh_layer_norm_nanobind.hpp"
 
-#include "pybind11/decorators.hpp"
-#include "pybind11/pytypes.h"
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+
+#include "ttnn-nanobind/decorators.hpp"
+
 #include "ttnn/operations/moreh/moreh_layer_norm/moreh_layer_norm.hpp"
 
+namespace nb = nanobind;
+
 namespace ttnn::operations::moreh::moreh_layer_norm {
-void bind_moreh_layer_norm_operation(py::module& module) {
+void bind_moreh_layer_norm_operation(nb::module_& mod) {
     bind_registered_operation(
-        module,
+        mod,
         ttnn::moreh_layer_norm,
         "Moreh Layer Norm Operation",
-        ttnn::pybind_arguments_t{
-            py::arg("input"),
-            py::arg("normalized_dims"),
-            py::arg("eps") = 1e-5f,
-            py::arg("gamma") = std::nullopt,
-            py::arg("beta") = std::nullopt,
-            py::kw_only(),
-            py::arg("output") = std::nullopt,
-            py::arg("mean") = std::nullopt,
-            py::arg("rstd") = std::nullopt,
-            py::arg("memory_config") = std::nullopt,
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{
+            nb::arg("input"),
+            nb::arg("normalized_dims"),
+            nb::arg("eps") = 1e-5f,
+            nb::arg("gamma") = std::nullopt,
+            nb::arg("beta") = std::nullopt,
+            nb::kw_only(),
+            nb::arg("output") = std::nullopt,
+            nb::arg("mean") = std::nullopt,
+            nb::arg("rstd") = std::nullopt,
+            nb::arg("memory_config") = std::nullopt,
+            nb::arg("compute_kernel_config") = std::nullopt});
 }
 }  // namespace ttnn::operations::moreh::moreh_layer_norm
--- ttnn/cpp/ttnn/operations/moreh/moreh_layer_norm/moreh_layer_norm_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_layer_norm/moreh_layer_norm_nanobind.hpp
@@ -4,10 +4,9 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
-
-namespace py = pybind11;
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::moreh::moreh_layer_norm {
-void bind_moreh_layer_norm_operation(py::module& module);
+namespace nb = nanobind;
+void bind_moreh_layer_norm_operation(nb::module_& mod);
 }  // namespace ttnn::operations::moreh::moreh_layer_norm
