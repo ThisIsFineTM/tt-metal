5e75ff86bf7d0e6e7da99ca6adb303671d0a03c7
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-23 22:07:49 -0400

    nb moreh_softmax_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/moreh/moreh_softmax/moreh_softmax_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_softmax/moreh_softmax_nanobind.cpp
@@ -2,32 +2,37 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "moreh_softmax_pybind.hpp"
+#include "moreh_softmax_nanobind.hpp"
+
+#include <optional>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
 
 #include "moreh_softmax.hpp"
-#include "pybind11/decorators.hpp"
-#include "cpp/pybind11/export_enum.hpp"
+#include "ttnn-nanobind/decorators.hpp"
+#include "cpp/ttnn-nanobind/export_enum.hpp"
 
 namespace ttnn::operations::moreh::moreh_softmax {
 
-void bind_moreh_softmax_operation(py::module& module) {
-    export_enum<MorehSoftmaxOp>(module, "MorehSoftmaxOp");
-    export_enum<MorehSoftmaxOpParallelizationStrategy>(module, "MorehSoftmaxOpParallelizationStrategy");
+void bind_moreh_softmax_operation(nb::module_& mod) {
+    export_enum<MorehSoftmaxOp>(mod, "MorehSoftmaxOp");
+    export_enum<MorehSoftmaxOpParallelizationStrategy>(mod, "MorehSoftmaxOpParallelizationStrategy");
 
 #define BIND_MOREH_SOFT_OP(op_name, op_enum, op_desc)                          \
     bind_registered_operation(                                                 \
-        module,                                                                \
+        mod,                                                                   \
         ttnn::op_name,                                                         \
         op_desc,                                                               \
-        ttnn::pybind_arguments_t{                                              \
-            py::arg("input_tensor"),                                           \
-            py::arg("dim"),                                                    \
-            py::kw_only(),                                                     \
-            py::arg("output_tensor") = std::nullopt,                           \
-            py::arg("op") = op_enum,                                           \
-            py::arg("strategy") = MorehSoftmaxOpParallelizationStrategy::NONE, \
-            py::arg("memory_config") = std::nullopt,                           \
-            py::arg("compute_kernel_config") = std::nullopt});
+        ttnn::nanobind_arguments_t{                                            \
+            nb::arg("input_tensor"),                                           \
+            nb::arg("dim"),                                                    \
+            nb::kw_only(),                                                     \
+            nb::arg("output_tensor") = std::nullopt,                           \
+            nb::arg("op") = op_enum,                                           \
+            nb::arg("strategy") = MorehSoftmaxOpParallelizationStrategy::NONE, \
+            nb::arg("memory_config") = std::nullopt,                           \
+            nb::arg("compute_kernel_config") = std::nullopt});
 
     BIND_MOREH_SOFT_OP(moreh_softmax, MorehSoftmaxOp::SOFTMAX, "Moreh Softmax Operation")
     BIND_MOREH_SOFT_OP(moreh_softmin, MorehSoftmaxOp::SOFTMIN, "Moreh Softmin Operation")
--- ttnn/cpp/ttnn/operations/moreh/moreh_softmax/moreh_softmax_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_softmax/moreh_softmax_nanobind.hpp
@@ -4,10 +4,10 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
-
-namespace py = pybind11;
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::moreh::moreh_softmax {
-void bind_moreh_softmax_operation(py::module& module);
+
+namespace nb = nanobind;
+void bind_moreh_softmax_operation(nb::module_& mod);
 }
