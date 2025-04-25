904aae24c1408da6c6a2cf9ee7946ff78269210f
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-25 17:47:17 -0400

    nb all_gather_matmul_nanobind
    * straight forward

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_matmul/all_gather_matmul_nanobind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_matmul/all_gather_matmul_nanobind.hpp
@@ -4,10 +4,11 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::operations::experimental::ccl {
 
-void py_bind_all_gather_matmul(pybind11::module& module);
+namespace nb = nanobind;
+void bind_all_gather_matmul(nb::module_& mod);
 
 }  // namespace ttnn::operations::experimental::ccl
