1290724128db4eef9922a0d406d0715d69a1e50c
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-25 19:52:09 -0400

    [WIP] ttnn-nanobind/tensor
    * placement new
    * readwrite -> rw, readonly -> ro
    * buffer / ownership model changed from pybind to nanobind.
    * nanobind does buffer stuff through ndarray

--- ttnn/cpp/ttnn-nanobind/tensor.hpp
+++ ttnn/cpp/ttnn-nanobind/tensor.hpp
@@ -4,15 +4,15 @@
 
 #pragma once
 
-#include "pybind11/pybind_fwd.hpp"
-
-namespace py = pybind11;
+#include "ttnn-nanobind/nanobind_fwd.hpp"
 
 namespace ttnn::tensor {
 
-void pytensor_module_types(py::module& m_tensor);
-void pytensor_module(py::module& m_tensor);
-void tensor_mem_config_module_types(py::module& module);
-void tensor_mem_config_module(py::module& module);
+namespace nb = nanobind;
+
+void pytensor_module_types(nb::module_& m_tensor);
+void pytensor_module(nb::module_& m_tensor);
+void tensor_mem_config_module_types(nb::module_& mod);
+void tensor_mem_config_module(nb::module_& mod);
 
 }  // namespace ttnn::tensor
