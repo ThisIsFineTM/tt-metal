55809ec1e047df18e43a1a378bd677e12ad79dc3
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-25 17:47:51 -0400

    all_gather_async_nanobind fix signature

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/all_gather_async_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/all_gather_async_nanobind.cpp
@@ -24,7 +24,7 @@ namespace ttnn::operations::experimental::ccl {
 namespace {
 
 template <typename ccl_operation_t>
-void bind_operation_all_gather_async(nanobind::module& module, const ccl_operation_t& operation, const char* doc) {
+void bind_operation_all_gather_async(nb::module_& mod, const ccl_operation_t& operation, const char* doc) {
 
     bind_registered_operation(
         mod,
