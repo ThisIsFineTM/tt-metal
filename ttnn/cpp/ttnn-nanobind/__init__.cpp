c2c2e04b3deb4853852aa19ca3ea68ebf517d115
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-24 21:35:29 -0400

    nb ttnn-nanobind/__init__
    * moved extra init code from header into cpp

--- ttnn/cpp/ttnn-nanobind/__init__.cpp
+++ ttnn/cpp/ttnn-nanobind/__init__.cpp
@@ -4,8 +4,9 @@
 
 #include "operations/__init__.hpp"
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <cstdint>
+
+#include <nanobind/nanobind.h>
 
 #include "activation.hpp"
 #include "cluster.hpp"
@@ -18,13 +19,176 @@
 #include "global_semaphore.hpp"
 #include "tensor.hpp"
 #include "reports.hpp"
-#include "ttnn/distributed/distributed_pybind.hpp"
+#include "ttnn/distributed/distributed_nanobind.hpp"
 #include "ttnn/deprecated/tt_lib/csrc/operations/primary/module.hpp"
-#include "ttnn/graph/graph_pybind.hpp"
+#include "ttnn/graph/graph_nanobind.hpp"
 #include "types.hpp"
 
-PYBIND11_MODULE(_ttnn, module) {
-    module.doc() = "Python bindings for TTNN";
+#include "ttnn-nanobind/types.hpp"
+#include "ttnn-nanobind/operations/copy.hpp"
+#include "ttnn-nanobind/operations/core.hpp"
+#include "ttnn-nanobind/operations/creation.hpp"
+#include "ttnn-nanobind/operations/trace.hpp"
+
+#include "ttnn/operations/bernoulli/bernoulli_nanobind.hpp"
+#include "cpp/ttnn/operations/ccl/ccl_nanobind.hpp"
+#include "ttnn/operations/conv/conv_nanobind.hpp"
+#include "ttnn/operations/data_movement/data_movement_nanobind.hpp"
+#include "ttnn/operations/eltwise/binary/binary_nanobind.hpp"
+#include "ttnn/operations/eltwise/binary_backward/binary_backward_nanobind.hpp"
+#include "ttnn/operations/eltwise/complex/complex_nanobind.hpp"
+#include "ttnn/operations/eltwise/complex_unary/complex_unary_nanobind.hpp"
+#include "ttnn/operations/eltwise/complex_unary_backward/complex_unary_backward_nanobind.hpp"
+#include "ttnn/operations/eltwise/quantization/quantization_nanobind.hpp"
+#include "ttnn/operations/eltwise/ternary/ternary_nanobind.hpp"
+#include "ttnn/operations/eltwise/ternary_backward/ternary_backward_nanobind.hpp"
+#include "ttnn/operations/eltwise/unary/unary_nanobind.hpp"
+#include "ttnn/operations/eltwise/unary_backward/unary_backward_nanobind.hpp"
+#include "ttnn/operations/embedding/embedding_nanobind.hpp"
+#include "ttnn/operations/embedding_backward/embedding_backward_nanobind.hpp"
+#include "ttnn/operations/examples/examples_nanobind.hpp"
+#include "ttnn/operations/experimental/experimental_nanobind.hpp"
+#include "ttnn/operations/full/full_nanobind.hpp"
+#include "ttnn/operations/full_like/full_like_nanobind.hpp"
+#include "ttnn/operations/index_fill/index_fill_nanobind.hpp"
+#include "ttnn/operations/kv_cache/kv_cache_nanobind.hpp"
+#include "ttnn/operations/loss/loss_nanobind.hpp"
+#include "ttnn/operations/matmul/matmul_nanobind.hpp"
+#include "ttnn/operations/moreh/moreh_nanobind.hpp"
+#include "ttnn/operations/normalization/normalization_nanobind.hpp"
+#include "ttnn/operations/pool/downsample/downsample_nanobind.hpp"
+#include "ttnn/operations/pool/generic/generic_pools_nanobind.hpp"
+#include "ttnn/operations/pool/global_avg_pool/global_avg_pool_nanobind.hpp"
+#include "ttnn/operations/pool/upsample/upsample_nanobind.hpp"
+#include "ttnn/operations/reduction/reduction_nanobind.hpp"
+#include "ttnn/operations/sliding_window/sliding_window_nanobind.hpp"
+#include "ttnn/operations/transformer/transformer_nanobind.hpp"
+#include "ttnn/operations/prefetcher/prefetcher_nanobind.hpp"
+#include "ttnn/operations/uniform/uniform_nanobind.hpp"
+
+namespace nb = nanobind;
+
+namespace ttnn::operations {
+
+void py_module(nb::module_& mod) {
+    auto m_core = mod.def_submodule("core", "core operations");
+    core::py_module_types(m_core);
+    core::py_module(m_core);
+
+    auto m_trace = mod.def_submodule("trace", "trace operations");
+    trace::py_module_types(m_trace);
+    trace::py_module(m_trace);
+
+    auto m_examples = mod.def_submodule("examples", "examples of operations");
+    examples::py_module(m_examples);
+
+    //  Eltwise operations: unary, binary, ternary, backward, complex
+    auto m_unary = mod.def_submodule("unary", "unary operations");
+    unary::py_module(m_unary);
+
+    auto m_binary = mod.def_submodule("binary", "binary operations");
+    binary::py_module(m_binary);
+
+    auto m_quantization = mod.def_submodule("quantization", "quantization operations");
+    quantization::py_module(m_quantization);
+
+    auto m_ternary = mod.def_submodule("ternary", "ternary operations");
+    ternary::py_module(m_ternary);
+
+    auto m_unary_backward = mod.def_submodule("unary_backward", "unary_backward operations");
+    unary_backward::py_module(m_unary_backward);
+
+    auto m_binary_backward = mod.def_submodule("binary_backward", "binary_backward operations");
+    binary_backward::py_module(m_binary_backward);
+
+    auto m_ternary_backward = mod.def_submodule("ternary_backward", "ternary_backward operations");
+    ternary_backward::py_module(m_ternary_backward);
+
+    auto m_complex = mod.def_submodule("complex", "complex tensor creation");
+    complex::py_module(m_complex);
+
+    auto m_complex_unary = mod.def_submodule("complex_unary", "complex_unary operations");
+    complex_unary::py_module(m_complex_unary);
+
+    auto m_complex_unary_backward = mod.def_submodule("complex_unary_backward", "complex_unary_backward operations");
+    complex_unary_backward::py_module(m_complex_unary_backward);
+
+    auto m_ccl = mod.def_submodule("ccl", "collective communication operations");
+    ccl::py_module(m_ccl);
+
+    auto m_creation = mod.def_submodule("creation", "creation operations");
+    creation::py_module(m_creation);
+
+    auto m_embedding = mod.def_submodule("embedding", "embedding operations");
+    embedding::py_module(m_embedding);
+
+    auto m_embedding_backward = mod.def_submodule("embedding_backward", "embedding backward operations");
+    embedding_backward::bind_embedding_backward(m_embedding_backward);
+
+    auto m_full = mod.def_submodule("full", "full operation");
+    full::bind_full_operation(m_full);
+
+    auto m_loss = mod.def_submodule("loss", "loss operations");
+    loss::bind_loss_functions(m_loss);
+
+    auto m_matmul = mod.def_submodule("matmul", "matmul operations");
+    matmul::py_module(m_matmul);
+
+    auto m_data_movement = mod.def_submodule("data_movement", "data_movement operations");
+    data_movement::py_module(m_data_movement);
+
+    auto m_sliding_window = mod.def_submodule("sliding_window", "sliding_window operations");
+    sliding_window::bind_sliding_window(m_sliding_window);
+
+    auto m_conv2d = mod.def_submodule("conv", "Convolution operations");
+    conv::py_module(m_conv2d);
+
+    auto m_pool = mod.def_submodule("pool", "pooling  operations");
+    pool::py_module(m_pool);
+    avgpool::py_module(m_pool);
+    upsample::py_module(m_pool);
+    downsample::bind_downsample(m_pool);
+
+    auto m_normalization = mod.def_submodule("normalization", "normalization operations");
+    normalization::py_module(m_normalization);
+
+    auto m_transformer = mod.def_submodule("transformer", "transformer operations");
+    transformer::py_module(m_transformer);
+
+    auto m_prefetcher = mod.def_submodule("prefetcher", "prefetcher operations");
+    prefetcher::py_module(m_prefetcher);
+
+    auto m_reduction = mod.def_submodule("reduction", "reduction operations");
+    reduction::py_module(m_reduction);
+
+    auto m_kv_cache = mod.def_submodule("kv_cache", "KV cache operations");
+    kv_cache::bind_kv_cache(m_kv_cache);
+
+    auto m_copy = mod.def_submodule("copy", "copy operations");
+    conb::py_module(m_copy);
+
+    auto m_experimental = mod.def_submodule("experimental", "experimental operations");
+    experimental::py_module(m_experimental);
+
+    auto m_moreh = mod.def_submodule("moreh", "moreh operations");
+    moreh::bind_moreh_operations(m_moreh);
+
+    auto m_full_like = mod.def_submodule("full_like", "full_like operation");
+    full_like::bind_full_like_operation(m_full_like);
+
+    auto m_uniform = mod.def_submodule("uniform", "uniform operations");
+    uniform::bind_uniform_operation(m_uniform);
+
+    auto m_index_fill = mod.def_submodule("index_fill", "index_fill operation");
+    index_fill::bind_index_fill_operation(m_index_fill);
+
+    auto m_bernoulli = mod.def_submodule("bernoulli", "bernoulli operations");
+    bernoulli::bind_bernoulli_operation(m_bernoulli);
+}
+}  // namespace ttnn::operations
+
+NB_MODULE(_ttnn, mod) {
+    mod.doc() = "Python bindings for TTNN";
 
     /*
     We have to make sure every class and enum is bound before any function that uses it as an argument or a return type.
@@ -34,26 +198,26 @@ PYBIND11_MODULE(_ttnn, module) {
     */
 
     // MODULES
-    auto m_deprecated = module.def_submodule("deprecated", "Deprecated tt_lib bindings");
-    auto m_tensor = module.def_submodule("tensor", "ttnn tensor");
+    auto m_deprecated = mod.def_submodule("deprecated", "Deprecated tt_lib bindings");
+    auto m_tensor = mod.def_submodule("tensor", "ttnn tensor");
 
     auto m_depr_operations = m_deprecated.def_submodule("operations", "Submodule for experimental operations");
     auto m_primary_ops = m_depr_operations.def_submodule("primary", "Primary operations");
 
-    auto m_graph = module.def_submodule("graph", "Contains graph capture functions");
-    auto m_types = module.def_submodule("types", "ttnn Types");
-    auto m_activation = module.def_submodule("activation", "ttnn Activation");
-    auto m_cluster = module.def_submodule("cluster", "ttnn cluster");
-    auto m_core = module.def_submodule("core", "core functions");
-    auto m_device = module.def_submodule("device", "ttnn devices");
-    auto m_multi_device = module.def_submodule("multi_device", "ttnn multi_device");
-    auto m_events = module.def_submodule("events", "ttnn events");
-    auto m_global_circular_buffer = module.def_submodule("global_circular_buffer", "ttnn global circular buffer");
-    auto m_global_semaphore = module.def_submodule("global_semaphore", "ttnn global semaphore");
-    auto m_profiler = module.def_submodule("profiler", "Submodule defining the profiler");
-    auto m_reports = module.def_submodule("reports", "ttnn reports");
-    auto m_operations = module.def_submodule("operations", "ttnn Operations");
-    auto m_fabric = module.def_submodule("fabric", "Fabric instantiation APIs");
+    auto m_graph = mod.def_submodule("graph", "Contains graph capture functions");
+    auto m_types = mod.def_submodule("types", "ttnn Types");
+    auto m_activation = mod.def_submodule("activation", "ttnn Activation");
+    auto m_cluster = mod.def_submodule("cluster", "ttnn cluster");
+    auto m_core = mod.def_submodule("core", "core functions");
+    auto m_device = mod.def_submodule("device", "ttnn devices");
+    auto m_multi_device = mod.def_submodule("multi_device", "ttnn multi_device");
+    auto m_events = mod.def_submodule("events", "ttnn events");
+    auto m_global_circular_buffer = mod.def_submodule("global_circular_buffer", "ttnn global circular buffer");
+    auto m_global_semaphore = mod.def_submodule("global_semaphore", "ttnn global semaphore");
+    auto m_profiler = mod.def_submodule("profiler", "Submodule defining the profiler");
+    auto m_reports = mod.def_submodule("reports", "ttnn reports");
+    auto m_operations = mod.def_submodule("operations", "ttnn Operations");
+    auto m_fabric = mod.def_submodule("fabric", "Fabric instantiation APIs");
 
     // TYPES
     ttnn::tensor::tensor_mem_config_module_types(m_tensor);
@@ -64,7 +228,7 @@ PYBIND11_MODULE(_ttnn, module) {
     ttnn::activation::py_module_types(m_activation);
     ttnn::core::py_module_types(m_core);
     ttnn::device::py_device_module_types(m_device);
-    ttnn::fabric::py_bind_fabric_api(m_fabric);
+    ttnn::fabric::bind_fabric_api(m_fabric);
     ttnn::distributed::py_module_types(m_multi_device);
     ttnn::events::py_module_types(m_events);
     ttnn::global_circular_buffer::py_module_types(m_global_circular_buffer);
@@ -78,7 +242,7 @@ PYBIND11_MODULE(_ttnn, module) {
     ttnn::graph::py_graph_module(m_graph);
 
 #if defined(TRACY_ENABLE)
-    py::function tracy_decorator = py::module::import("tracy.ttnn_profiler_wrapper").attr("callable_decorator");
+    nb::function tracy_decorator = nb::module::import("tracy.ttnn_profiler_wrapper").attr("callable_decorator");
 
     tracy_decorator(m_device);
     tracy_decorator(m_tensor);
@@ -102,37 +266,37 @@ PYBIND11_MODULE(_ttnn, module) {
     ttnn::operations::py_module(m_operations);
     tt::operations::primary::py_module(m_primary_ops);
 
-    module.attr("CONFIG") = &ttnn::CONFIG;
-    module.def(
+    mod.attr("CONFIG") = &ttnn::CONFIG;
+    mod.def(
         "get_python_operation_id",
         []() -> std::uint64_t { return ttnn::CoreIDs::instance().get_python_operation_id(); },
         "Get operation id");
-    module.def(
+    mod.def(
         "set_python_operation_id",
         [](std::uint64_t id) { ttnn::CoreIDs::instance().set_python_operation_id(id); },
         "Set operation id");
-    module.def(
+    mod.def(
         "fetch_and_increment_python_operation_id",
         []() -> std::uint64_t { return ttnn::CoreIDs::instance().fetch_and_increment_python_operation_id(); },
         "Increment tensor id and return the previously held id");
 
-    module.def(
+    mod.def(
         "get_tensor_id", []() -> std::uint64_t { return ttnn::CoreIDs::instance().get_tensor_id(); }, "Get tensor id");
-    module.def("set_tensor_id", [](std::uint64_t id) { ttnn::CoreIDs::instance().set_tensor_id(id); }, "Set tensor id");
-    module.def(
+    mod.def("set_tensor_id", [](std::uint64_t id) { ttnn::CoreIDs::instance().set_tensor_id(id); }, "Set tensor id");
+    mod.def(
         "fetch_and_increment_tensor_id",
         []() -> std::uint64_t { return ttnn::CoreIDs::instance().fetch_and_increment_tensor_id(); },
         "Increment tensor id and return the previously held id");
 
-    module.def(
+    mod.def(
         "get_device_operation_id",
         []() -> std::uint64_t { return ttnn::CoreIDs::instance().get_device_operation_id(); },
         "Get device operation id");
-    module.def(
+    mod.def(
         "set_device_operation_id",
         [](std::uint64_t id) { ttnn::CoreIDs::instance().set_device_operation_id(id); },
         "Set device operation id");
-    module.def(
+    mod.def(
         "fetch_and_increment_device_operation_id",
         []() -> std::uint64_t { return ttnn::CoreIDs::instance().fetch_and_increment_device_operation_id(); },
         "Increment device operation id and return the previously held id");
