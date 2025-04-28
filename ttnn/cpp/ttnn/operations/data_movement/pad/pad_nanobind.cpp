b906a4a96027fb5e54b8917c1297c9de871f2cba
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-04-28 16:23:29 -0400

    pad_nanobind fix rebase

--- ttnn/cpp/ttnn/operations/data_movement/pad/pad_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/pad/pad_nanobind.cpp
@@ -26,27 +26,27 @@ void bind_pad(nb::module_& mod) {
         R"doc(
 
             Returns a padded tensor, with a specified value at the specified location. If the input tensor is on host, the pad will be performed on host, and if its on device it will be performed on device.
-
-            Equivalent pytorch code:
-
-            .. code-block:: python
-
-                torch.pad(input_tensor, padding, value)
-                torch.pad(input_tensor, output_tensor_shape, input_tensor_start, value)
+            Any rank of tensor is supported, however tensors with rank > 4 can only apply padding to the lower 3 dimensions.
 
             Args:
-                input_tensor (ttnn.Tensor): the input tensor.
-                padding (ttnn.Tensor): padding to apply. Each element of padding should be a tuple of 2 integers, with the first integer specifying the number of values to add before the tensor and the second integer specifying the number of values to add after the tensor. Mutually exclusive to output_tensor_shape and input_tensor_start.
-                output_tensor_shape (shape): Final shape of padded tensor. This along with input_tensor_start is mutually exclusive from padding.
-                input_tensor_start (shape): Shape describing where to start padding. This along with output_tensor_shape is mutually exclusive from padding.
-                value (number): value to pad with.
+                * :attr:`input_tensor`: (ttnn.Tensor): the input tensor.
+                * :attr:`padding`: (list[Tuple[int,int]]): padding to apply. Each element of padding should be a tuple of 2 integers, with the first integer specifying the number of values to add before the tensor and the second integer specifying the number of values to add after the tensor. Mutually exclusive to output_tensor_shape and input_tensor_start.
+                * :attr:`value`: (Union[float,int]): value to pad with.
 
             Keyword Args:
-                memory_config (ttnn.MemoryConfig, optional): Memory configuration for the operation. Defaults to `None`.
-                queue_id (int, optional): command queue id. Defaults to `0`.
+                * :attr:`use_multicore`: (Optional[bool]) switch to use multicore implementation
+                * :attr:`memory_config`: (Optional[ttnn.MemoryConfig]): Memory configuration for the operation. Defaults to `None`.
+                * :attr:`queue_id`: (Optional[int]): command queue id. Defaults to `0`.
 
             Returns:
                List of ttnn.Tensor: the output tensor.
+
+            Example:
+                .. code-block:: python
+
+                    input_tensor = ttnn.pad(pad_input, [(0, 0), (0, 0), (0, 12), (0, 12)], 0)
+                    assert (ttnn.to_torch(input_tensor[:, :, 20:32, 20:32]) == 0).all()
+                    assert input_tensor.shape == Shape([1, 8, 32, 32])
 
         )doc";
 
@@ -68,87 +68,6 @@ void bind_pad(nb::module_& mod) {
             nb::arg("value"),
             nb::kw_only(),
             nb::arg("use_multicore") = true,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array1D& output_padded_shape,
-               const tt::tt_metal::Array1D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array2D& output_padded_shape,
-               const tt::tt_metal::Array2D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array3D& output_padded_shape,
-               const tt::tt_metal::Array3D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
             nb::arg("memory_config") = std::nullopt,
             nb::arg("queue_id") = DefaultQueueId,
         },
@@ -177,115 +96,6 @@ void bind_pad(nb::module_& mod) {
             nb::kw_only(),
             nb::arg("use_multicore") = false,
             nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array5D& output_padded_shape,
-               const tt::tt_metal::Array5D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array6D& output_padded_shape,
-               const tt::tt_metal::Array6D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array7D& output_padded_shape,
-               const tt::tt_metal::Array7D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        },
-        ttnn::nanobind_overload_t{
-            [](const OperationType& self,
-               const ttnn::Tensor& input_tensor,
-               const tt::tt_metal::Array8D& output_padded_shape,
-               const tt::tt_metal::Array8D& input_tensor_start,
-               const float value,
-               const bool use_multicore,
-               const std::optional<ttnn::MemoryConfig>& memory_config,
-               QueueId queue_id) {
-                return self(
-                    queue_id,
-                    input_tensor,
-                    output_padded_shape,
-                    input_tensor_start,
-                    value,
-                    use_multicore,
-                    memory_config);
-            },
-            nb::arg("input_tensor"),
-            nb::arg("output_padded_shape"),
-            nb::arg("input_tensor_start"),
-            nb::arg("value"),
-            nb::kw_only(),
-            nb::arg("use_multicore") = false,
-            nb::arg("memory_config") = std::nullopt,
-            nb::arg("queue_id") = DefaultQueueId,
-        });
+            nb::arg("queue_id") = DefaultQueueId});
 }
 }  // namespace ttnn::operations::data_movement::detail
